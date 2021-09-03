06836693cc11b0c28c4c89f4ab50824b693a657a
Charlie Fish <fishcharlie@me.com>
2021-09-02 21:06:45 -0600

    Improving timeTo24HourTime to allow for more values

--- code/utils/timeTo24HourTime.js
+++ code/utils/timeTo24HourTime.js
@@ -1,4 +1,19 @@
+// 11:00 AM
+// 11:00AM
+// 5:00am
 module.exports = (timeString) => {
+	timeString = timeString.trim().toUpperCase();
+
+	if (!timeString.includes(" ")) {
+		if (timeString.endsWith("PM")) {
+			timeString = timeString.replace("PM", " PM");
+		} else if (timeString.endsWith("AM")) {
+			timeString = timeString.replace("AM", " AM");
+		} else {
+			throw new Error(`Can't parse AM or PM for ${timeString}`);
+		}
+	}
+
 	const [time, ampm] = timeString.split(" ");
 	const [hour, minute] = time.split(":");
 
